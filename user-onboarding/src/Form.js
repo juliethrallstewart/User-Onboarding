fe78d46474bb1df0194e2e9fc2c5ecf217efa3fd
Julie Thrall <juliethrall10@gmail.com>
2019-08-07 16:11:27 -0700

    end of day commit

---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Form as FormikForm, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+const Form = ({ errors, touched, values, handleSubmit, status }) => {
+	const [
+		users,
+		setUser
+	] = useState([]);
+
+	console.log('this is the status', status);
+	console.log('this is the users list', users);
+
+	return (
+		<div className="user-form">
+			<h1>User Onboarding</h1>
+			<FormikForm>
+				<Field type="text" name="name" placeholder="Name" />
+				{touched.name && errors.name && <p className="error">{errors.name}</p>}
+
+				<Field type="text" name="email" placeholder="Email" />
+				{touched.email && errors.email && <p className="error">{errors.email}</p>}
+
+				<Field type="text" name="password" placeholder="Password" />
+				{touched.email && errors.email && <p className="error">{errors.email}</p>}
+
+				<label className="checkbox-container">
+					<p className="terms-service">Terms of Service</p>
+					<Field type="checkbox" name="terms" checked={values.terms} />
+					<span className="checkmark" />
+				</label>
+
+				<button type="submit">Submit!</button>
+			</FormikForm>
+		</div>
+	);
+};
+const FormikUserForm = withFormik({
+	mapPropsToValues ({ name, email, password, terms }) {
+		return {
+			terms    : terms || false,
+			email    : email || '',
+			name     : name || '',
+			password : password || ''
+		};
+	},
+
+	validationSchema : Yup.object().shape({
+		name     : Yup.string().required('You cannot pass!!!'),
+		email    : Yup.string().email('Enter a VALID email').required('You want in, you gotta pay to play'),
+		password : Yup.string().min(6, 'Password has to be longer than 6 characters').required('Cannot pass'),
+		terms    : Yup.bool()
+			.test('consent', 'You have to agree!', (values) => values === true)
+			.required('Just check the box..')
+	}),
+
+	handleSubmit (values, { setStatus, status, setUser, users }) {
+		axios
+			.post('https://reqres.in/api/users/', values)
+			.then((res) => {
+				setStatus(res.data);
+			})
+			.then((res) => {
+				setUser({ ...users, status });
+			})
+			.catch((err) => console.log(err.response));
+	}
+})(Form);
+
+export default FormikUserForm;
